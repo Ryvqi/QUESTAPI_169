d5e904f8ef925243506788dbfbd1235550388567
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-30 02:18:03 +0700

    menambahkan object PenyediaViewModel

--- app/src/main/java/com/example/week12/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/week12/ui/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,11 @@
 package com.example.week12.ui.viewmodel
 
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiKontak().container.kontakRepository) }
+        initializer { InsertViewModel(aplikasiKontak().container.kontakRepository) }
+    }
+}
