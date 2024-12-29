6bdf3f7af768ac7c04aac55e7d55738744c3bdbf
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-29 22:58:55 +0700

    menambahkan sealed class HomeUiState untuk mengelola status ui

--- app/src/main/java/com/example/week12/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/week12/ui/viewmodel/HomeViewModel.kt
@@ -1,2 +1,11 @@
 package com.example.week12.ui.viewmodel
 
+import com.example.week12.model.Mahasiswa
+
+sealed class HomeUiState{
+    data class Success(
+        val mahasiwa: List<Mahasiswa>
+    ): HomeUiState()
+    object Error: HomeUiState()
+    object Loading: HomeUiState()
+}
