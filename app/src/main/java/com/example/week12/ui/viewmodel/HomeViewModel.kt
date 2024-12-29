8845928a45accb43436b3e39ba069eca38db8270
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-29 23:11:34 +0700

    menambahkan HomeViewModel dengan MahasiswaRepository dependensi

--- app/src/main/java/com/example/week12/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/week12/ui/viewmodel/HomeViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.week12.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.week12.model.Mahasiswa
+import com.example.week12.repository.MahasiswaRepository
 
 sealed class HomeUiState{
     data class Success(
@@ -8,4 +13,9 @@ sealed class HomeUiState{
     ): HomeUiState()
     object Error: HomeUiState()
     object Loading: HomeUiState()
+}
+
+class HomeViewModel (private val mhs: MahasiswaRepository): ViewModel(){
+    var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
 }
