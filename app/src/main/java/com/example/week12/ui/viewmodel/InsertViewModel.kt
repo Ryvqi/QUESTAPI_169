d97543e4833c5b19e3a78c4f0ab237703539dabc
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-30 00:49:29 +0700

    menambahkan InsertViewModel dengan MahasiswaRepository dependensi

--- app/src/main/java/com/example/week12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/week12/ui/viewmodel/InsertViewModel.kt
@@ -1,6 +1,16 @@
 package com.example.week12.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.week12.model.Mahasiswa
+import com.example.week12.repository.MahasiswaRepository
+
+class InsertViewModel (private val mhs: MahasiswaRepository): ViewModel(){
+    var uiState by mutableStateOf(InserUiState())
+        private set
+}
 
 data class InserUiState(
     val inserUiEvent: InsertUiEvent = InsertUiEvent()
