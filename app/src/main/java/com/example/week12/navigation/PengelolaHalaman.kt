97dc6ff759f10630c72be92c3e0d41272e1b9b82
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-30 02:25:38 +0700

    menambahkan fungsi PengelolaHalaman

--- app/src/main/java/com/example/week12/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/week12/navigation/PengelolaHalaman.kt
@@ -1,2 +1,38 @@
 package com.example.week12.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.week12.ui.view.DestinasiEntry
+import com.example.week12.ui.view.DestinasiHome
+import com.example.week12.ui.view.EntryMhsScreen
+import com.example.week12.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ) {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
