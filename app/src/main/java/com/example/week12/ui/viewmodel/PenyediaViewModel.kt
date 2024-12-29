fdf3f4b87d0621330116b509c384bf36b2d7f7fe
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-30 02:20:34 +0700

    menambahkan fungsi ekstensi untuk mengambil mahasiswaapllication dari creationextras

--- app/src/main/java/com/example/week12/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/week12/ui/viewmodel/PenyediaViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.week12.ui.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.week12.MahaiswaApplication
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
@@ -9,3 +12,6 @@ object PenyediaViewModel{
         initializer { InsertViewModel(aplikasiKontak().container.kontakRepository) }
     }
 }
+
+fun CreationExtras.aplikasiKontak(): MahaiswaApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahaiswaApplication)
