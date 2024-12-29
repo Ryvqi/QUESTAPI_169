7d8e660c2cef396307d619db5dd8378a593a30a8
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-30 00:34:06 +0700

    menambahkan data class InsertUiEvent

--- app/src/main/java/com/example/week12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/week12/ui/viewmodel/InsertViewModel.kt
@@ -3,3 +3,12 @@ package com.example.week12.ui.viewmodel
 data class InserUiState(
     val inserUiState: InserUiState = InsertUiEvent()
 )
+
+data class InsertUiEvent(
+    val nim:String = "",
+    val nama:String = "",
+    val alamat:String = "",
+    val jenisKelamin:String = "",
+    val kelas:String = "",
+    val angkatan:String = "",
+)
