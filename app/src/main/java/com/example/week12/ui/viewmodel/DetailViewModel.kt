b6a01f331264f98414e153b684922b456ac3bff8
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-30 10:55:30 +0700

    menambahkan DetailViewModel dengan mahasiswa repository dependensi

--- app/src/main/java/com/example/week12/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/week12/ui/viewmodel/DetailViewModel.kt
@@ -1,11 +1,27 @@
 package com.example.week12.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.week12.model.Mahasiswa
+import com.example.week12.repository.MahasiswaRepository
 
 sealed class DetailUiState{
     data class Success(
         val mahasiwa: Mahasiswa
-    ): HomeUiState()
-    object Error: HomeUiState()
-    object Loading: HomeUiState()
+    ): DetailUiState()
+    object Error: DetailUiState()
+    object Loading: DetailUiState()
+}
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mhs: MahasiswaRepository
+): ViewModel(){
+    var mhsUiState: DetailUiState by mutableStateOf(DetailUiState.Loading)
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
 }
