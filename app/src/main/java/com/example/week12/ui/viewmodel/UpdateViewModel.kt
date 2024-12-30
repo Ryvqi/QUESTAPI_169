e0c16560a88c081e582c14e0b9e9c404007078fd
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-30 12:23:50 +0700

    menambahkan class UpdateViewModel

--- app/src/main/java/com/example/week12/ui/viewmodel/UpdateViewModel.kt
+++ app/src/main/java/com/example/week12/ui/viewmodel/UpdateViewModel.kt
@@ -1,2 +1,18 @@
 package com.example.week12.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.week12.repository.MahasiswaRepository
+
+class UpdateViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val mhs: MahasiswaRepository
+): ViewModel(){
+    var uiState by mutableStateOf(InserUiState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+}
