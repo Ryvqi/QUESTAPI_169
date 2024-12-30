64114a7ca525e9c18d62d1fe41dc2a5d93e6476b
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-30 12:10:48 +0700

    menambahkan fungsi DetailScreen

--- app/src/main/java/com/example/week12/ui/view/DetailView.kt
+++ app/src/main/java/com/example/week12/ui/view/DetailView.kt
@@ -1,25 +1,84 @@
 package com.example.week12.ui.view
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.week12.model.Mahasiswa
 import com.example.week12.navigation.DestinasiNavigasi
+import com.example.week12.ui.CostumeTopAppBar
+import com.example.week12.ui.viewmodel.DetailUiState
+import com.example.week12.ui.viewmodel.DetailViewModel
+import com.example.week12.ui.viewmodel.PenyediaViewModel
 
 object DestinasiDetail: DestinasiNavigasi{
     override val route = "detail"
     override val titleRes = "Detail Mhs"
     const val NIM = "nim"
     val routeWithArg = "$route/{$NIM}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(
+    modifier: Modifier = Modifier,
+    navigateBack: () -> Unit,
+    onUpdateClick: (String) -> Unit = {},
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    Scaffold(
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiDetail.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {onUpdateClick(viewModel.mhsUiState.toString())},
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Edit Mahasiswa")
+            }
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding),
+            contentAlignment = Alignment.BottomEnd
+        ){
+            when(val state = viewModel.mhsUiState){
+                is DetailUiState.Loading -> {
+                    CircularProgressIndicator()
+                }
+                is DetailUiState.Error -> {
+                    CircularProgressIndicator()
+                }
+                is DetailUiState.Success -> {
+                    ItemDetail(mahasiswa = state.mahasiwa)
+                }
+            }
+        }
+    }
 }
 
 @Composable
