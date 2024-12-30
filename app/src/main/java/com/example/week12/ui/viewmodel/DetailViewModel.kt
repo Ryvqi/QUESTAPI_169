d425e3795fa9eff45ccf6cc160cd25ced7804159
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-30 10:26:13 +0700

    menambahkan sealed class untuk mengelola status ui

--- app/src/main/java/com/example/week12/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/week12/ui/viewmodel/DetailViewModel.kt
@@ -1,2 +1,11 @@
 package com.example.week12.ui.viewmodel
 
+import com.example.week12.model.Mahasiswa
+
+sealed class DetailUiState{
+    data class Success(
+        val mahasiwa: Mahasiswa
+    ): HomeUiState()
+    object Error: HomeUiState()
+    object Loading: HomeUiState()
+}
