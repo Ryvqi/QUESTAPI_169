93a46f50150aed297b2c5e703e2de3fcf36f3535
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-30 12:49:48 +0700

    menambahkan object DestinasiUpdate

--- app/src/main/java/com/example/week12/ui/view/UpdateView.kt
+++ app/src/main/java/com/example/week12/ui/view/UpdateView.kt
@@ -1,2 +1,10 @@
 package com.example.week12.ui.view
 
+import com.example.week12.navigation.DestinasiNavigasi
+
+object DestinasiUpdate: DestinasiNavigasi{
+    override val route = "update"
+    override val titleRes = "Update Mhs"
+    const val NIM = "nim"
+    val routeWithArg = "$route/{$NIM}"
+}
