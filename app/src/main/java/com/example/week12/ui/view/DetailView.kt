ebc30ddeb977fc9971c7cd279919e5db1d58a53a
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-30 11:07:59 +0700

    menambahkan fungsi ComponentDetail

--- app/src/main/java/com/example/week12/ui/view/DetailView.kt
+++ app/src/main/java/com/example/week12/ui/view/DetailView.kt
@@ -1,2 +1,31 @@
 package com.example.week12.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ComponentDetail(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            style = MaterialTheme.typography.titleMedium
+        )
+
+        Text(
+            text = isinya,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
