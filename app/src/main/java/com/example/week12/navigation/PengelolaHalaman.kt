5d73a036b0490fd1fd02ba453bdef9cadc7f79b4
Ryvqi <rifqikhabibi03@gmail.com>
2024-12-30 13:28:43 +0700

    menambahkan PengelolaHalaman

--- app/src/main/java/com/example/week12/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/week12/navigation/PengelolaHalaman.kt
@@ -6,10 +6,14 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.week12.ui.view.DestinasiDetail
 import com.example.week12.ui.view.DestinasiEntry
 import com.example.week12.ui.view.DestinasiHome
+import com.example.week12.ui.view.DestinasiUpdate
+import com.example.week12.ui.view.DetailScreen
 import com.example.week12.ui.view.EntryMhsScreen
 import com.example.week12.ui.view.HomeScreen
+import com.example.week12.ui.view.UpdateScreen
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
@@ -21,7 +25,8 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
         composable(DestinasiHome.route){
             HomeScreen(
                 navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
-                onDetailClick = {
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
                 }
             )
         }
@@ -33,6 +38,19 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                     }
                 }
             })
+        }
+        composable(DestinasiDetail.routeWithArg){backStackEntry ->
+            val nim = backStackEntry.arguments?.getString(DestinasiDetail.NIM) ?: ""
+            DetailScreen(
+                navigateBack = {navController.popBackStack()},
+                onUpdateClick = {navController.navigate("${DestinasiUpdate.route}/$nim")}
+            )
+        }
+        composable(DestinasiUpdate.routeWithArg) { backStackEntry ->
+            val nim = backStackEntry.arguments?.getString(DestinasiUpdate.NIM) ?: ""
+            UpdateScreen(
+                navigateBack = { navController.popBackStack() },
+            )
         }
     }
 }
